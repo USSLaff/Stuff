291c4469fd3dff88a6ca9cec2bf073767bdd3fb5
USSLaff <gerreger0170@gmail.com>
2023-09-11 11:17:39 +0200

    1, 2, 3, kész.

--- sudokuCLI/sudokuCLI/Program.cs
+++ sudokuCLI/sudokuCLI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace sudokuCLI
 {
@@ -10,6 +11,19 @@ namespace sudokuCLI
     {
         static void Main(string[] args)
         {
+            string[] data = File.ReadAllLines("feladvanyok.txt");
+            Console.WriteLine();
+            Console.WriteLine("Adj meg egy számot 4 és 9 között!");
+            int input = getUInput();
+
+
+
+            Console.ReadKey();
+        }
+        public static int getUInput()
+        {
+            return 0;
         }
     }
+
 }

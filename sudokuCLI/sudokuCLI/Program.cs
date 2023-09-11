5fd035213cc7afe3c534a2a24b49328a61edf8ef
USSLaff <gerreger0170@gmail.com>
2023-09-11 11:47:56 +0200

    6. kész

--- sudokuCLI/sudokuCLI/Program.cs
+++ sudokuCLI/sudokuCLI/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.Remoting.Messaging;
 using System.Globalization;
+using System.Xml.Schema;
 
 namespace sudokuCLI
 {
@@ -28,14 +29,19 @@ namespace sudokuCLI
             List<string> filtered = new List<string>();
             foreach (string item in data)
             {
-                filtered.Add(item);
+                if (item.Length==size)
+                {
+                    filtered.Add(item);
+                }
             }
             string pick = filtered[rnd.Next(0, filtered.Count)];
             Console.WriteLine($"5. Feladat: A kiválasztot feladvány:\n{pick}");
 
             //6.
-            int fillCount = pick.Count(x=>!x.Equals('0'));
-            Console.WriteLine($"max: {size} töltött: {fillCount}");
+            int fillCount = pick.Count(x => !x.Equals('0'));
+            Console.WriteLine($"t:{fillCount} max:{size}");
+            Console.WriteLine($"6. Feladat: A feladvány kitöltöttsége: {Math.Round((fillCount/(float)size)*100)}%");
+
 
 
             Console.ReadKey();
